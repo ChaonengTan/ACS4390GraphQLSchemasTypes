fb6668b443e12541b515a4960195e75ccf11927b
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-01-20 18:49:44 -0800

    +game, time, random

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 // mockDB
 const petList = require('./petList.json')
+const gameData = require('./gameData.json')
 
 // schema
 const schema = buildSchema(`
@@ -22,11 +23,40 @@ const schema = buildSchema(`
         name: String!
         species: String!
     }
+    type Game {
+        index: Int!
+        game: String!
+        score: Int!
+        leaderboard: String!
+        gamers: String!
+        comp_perc: String!
+        rating: String!
+        url: String!
+        min_comp_time: String!
+        max_comp_time: String!
+    }
+    enum TimeSettings {
+        hours
+        minutes
+        seconds
+    }
+    type Time {
+        time: Int
+    }
+    type Number {
+        number: Int
+    }
     type Query {
         getAbout: About
         getMeal(time: MealTime!): Meal
         getPet(id: Int!): Pet
         allPets: [Pet!]! 
+        getGame(index: Int!): Game
+        allGames: [Game!]!
+        firstGame: Game
+        lastGame: Game
+        getTime(type: TimeSettings!): Time
+        getRandom(range: Int!): Number
     }
 `)
 
@@ -46,6 +76,26 @@ const root = {
 	allPets: () => {	
 		return petList
 	},
+    getGame: ({ index }) => {
+        return gameData[index]
+    },
+    allGames: () => {
+        return gameData
+    },
+    firstGame: () => {
+        return gameData[0]
+    },
+    lastGame: () => {
+        return gameData[gameData.length-1]
+    },
+    getTime: ({ type }) => {
+        const date = new Date()
+        const allTypes = { hours: date.getHours(), minutes: date.getMinutes(), seconds: date.getSeconds() }
+        return { time: allTypes[type] }
+    },
+    getRandom: ({ range }) => {
+        return { number: Math.floor(Math.random() * range) }
+    }
 }
 
 // app
